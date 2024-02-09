5302505f41ccb576b2fba1364fc2d7f9614c48c7
NutThai <88415649+NutThai@users.noreply.github.com>
2024-02-10 03:36:58 +0700

    update shopping cart

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -20,7 +20,7 @@
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <a v-for="item in navigation" :key="item.name" :href="item.href"
-                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base', 'font[\'kanit\']']"
                 :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
             </div>
           </div>
@@ -72,7 +72,7 @@
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
         <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-[\'kanit\']']"
           :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
       </div>
     </DisclosurePanel>
@@ -91,25 +91,66 @@
               enter-from="translate-x-full" enter-to="translate-x-0"
               leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0"
               leave-to="translate-x-full">
-              <DialogPanel class="pointer-events-auto relative w-screen max-w-md">
-                <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0"
-                  enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
-                  <div class="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                    <button type="button"
-                      class="relative rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
-                      @click="openCart = false">
-                      <span class="absolute -inset-2.5" />
-                      <span class="sr-only">ปิด</span>
-                      <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-                    </button>
+              <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                  <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                    <div class="flex items-start justify-between">
+                      <DialogTitle class="text-3xl font-['kanit'] text-gray-900">ตะกร้าสินค้า</DialogTitle>
+                      <div class="ml-3 flex h-7 items-center">
+                        <button type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
+                          @click="openCart = false">
+                          <span class="absolute -inset-0.5" />
+                          <span class="sr-only">Close panel</span>
+                          <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+
+                    <div class="mt-8">
+                      <div class="flow-root">
+                        <ul role="list" class="-my-6 divide-y divide-gray-200">
+                          <li v-for="product in products" :key="product.id" class="flex py-6">
+                            <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                              <img :src="product.imageSrc" :alt="product.imageAlt"
+                                class="h-full w-full object-cover object-center" />
+                            </div>
+
+                            <div class="ml-4 flex flex-1 flex-col">
+                              <div>
+                                <div class="flex justify-between text-base font-['kanit'] text-gray-900">
+                                  <h3>
+                                    <a :href="product.href">{{ product.name }}</a>
+                                  </h3>
+                                  <p class="ml-4">{{ product.price }}</p>
+                                </div>
+                                <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                              </div>
+                              <div class="flex flex-1 items-end justify-between text-sm">
+                                <p class="text-gray-500">Qty {{ product.quantity }}</p>
+
+                                <div class="flex">
+                                  <button type="button"
+                                    class="font-['kanit'] text-indigo-600 hover:text-indigo-500">Remove</button>
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
-                </TransitionChild>
-                <div class="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                  <div class="px-4 sm:px-6">
-                    <DialogTitle class="text-xl font-['kanit'] leading-6 text-gray-900">ตะกร้าสินค้า</DialogTitle>
-                  </div>
-                  <div class="relative mt-6 flex-1 px-4 sm:px-6">
-                    <!-- Your content -->
+
+                  <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
+                    <div class="flex justify-between text-base font-['kanit'] text-gray-900">
+                      <p>Subtotal</p>
+                      <p>$262.00</p>
+                    </div>
+                    <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                    <div class="mt-6">
+                      <a href="#"
+                        class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-['kanit'] text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+                    </div>
+
                   </div>
                 </div>
               </DialogPanel>
@@ -128,6 +169,30 @@ import { } from '@headlessui/vue'
 import { ref } from 'vue'
 
 const openCart = ref(false)
+const products = [
+  {
+    id: 1,
+    name: 'Throwback Hip Bag',
+    href: '#',
+    color: 'Salmon',
+    price: '$90.00',
+    quantity: 1,
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+  },
+  {
+    id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+  // More products...
+]
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
