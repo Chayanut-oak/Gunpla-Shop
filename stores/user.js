660f91840cb89d7c0c442bc63f0646ab247d3f6c
Chayanut-oak <chayanut.oakley@gmail.com>
2024-03-05 17:17:53 +0700

    regis

--- stores/user.js
+++ stores/user.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+
 export const useUserStore = defineStore('user', {
     state: () => ({
         token: "",
@@ -13,12 +14,40 @@ export const useUserStore = defineStore('user', {
     },
     actions: {
         async login(username, password) {
+            const { $api } = useNuxtApp()
             try {
                 const requestBody = {
                     email: username,
                     password: password
                 };
-                const response = await fetch('http://localhost:8080/user/authentication', {
+                const response = await $api('/user/authentication', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(requestBody)
+                });
+                  console.log(response.json())
+                if (!response.ok) {
+                    throw new Error('Failed to login');
+                }   
+               
+                const data = await response.json(); 
+           
+                this.token = data;
+            } catch (error) {
+                console.error('Error logging in:', error);
+                throw error;
+            }
+        },
+        async register(username, password) {
+            const { $api } = useNuxtApp()
+            try {
+                const requestBody = {
+                    email: username,
+                    password: password
+                };
+                const response = await $api('/user/newUser', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -35,7 +64,6 @@ export const useUserStore = defineStore('user', {
                 throw error;
             }
         }
-        
     }
 
 })
