2d0585e9f2de977c4c72e97ef513d5807c42babf
NutThai <88415649+NutThai@users.noreply.github.com>
2024-03-09 18:05:16 +0700

    fix url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default {
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL || "http://localhost:8080",
+      baseURL: process.env.BASE_URL || "http://localhost/api",
     },
   },
   modules: [
